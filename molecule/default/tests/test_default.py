591927b0c2e36de21656fff2af1333fd857e315f
14301253Z <asg1612@gmail.com>
2017-11-10 08:41:47 +0100

    fix meta and add test socket

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_registry_socket(host):
+    assert host.socket('tcp://0.0.0.0:5000').is_listening
